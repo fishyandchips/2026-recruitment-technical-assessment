917534b9687236addd4a8649ffd5d2180fb6dc57
Brian Wifanie <brianwifanie@gmail.com>
2026-02-14 13:47:46 +1100

    Completed backend tasks in python

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,25 +43,109 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	recipeName = recipeName.replace("-", " ").replace("_", " ")
+	recipeName = re.sub(r'[^a-zA-Z ]+', "", recipeName)
+	recipeName = re.sub(r'\s+', " ", recipeName)
+	recipeName = recipeName.strip()
+
+	return recipeName.title() if len(recipeName) > 0 else None
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.json
+	entryType, name, requiredItems, cookTime = (
+		data.get("type"),
+		data.get("name"),
+		data.get("requiredItems"),
+		data.get("cookTime"),
+	)
+
+	if entryType not in ["recipe", "ingredient"]:
+		return "Type can only be recipe or ingredient.", 400
+
+	if entryType == "ingredient" and cookTime < 0:
+		return "cookTime can only be greater than or equal to 0.", 400
+	
+	if name in cookbook:
+		return "Entry names must be unique.", 400
+
+	if entryType == "recipe":
+		requiredSet = set()
+		for item in requiredItems:
+			requiredSet.add(item.get("name"))
+
+		if len(requiredSet) != len(requiredItems):
+			return "Recipe requiredItems can only have one element per name.", 400
+
+	cookbook[name] = data
+	return {}, 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	name = request.args.get("name")
 
+	if name not in cookbook:
+		return "A recipe with the corresponding name cannot be found.", 400
+
+	entry = cookbook[name]
+
+	if entry.get("type") != "recipe":
+		return "The searched name is NOT a recipe name (ie. an ingredient).", 400
+
+	def dfs(entry, multiplier=1):
+		totalCookTime = 0
+		totalIngredients = {}
+
+		for requiredItem in entry.get("requiredItems"):
+			itemName = requiredItem.get("name")
+			quantity = requiredItem.get("quantity")
+
+			if itemName not in cookbook:
+				raise ValueError("The recipe contains recipes or ingredients that aren't in the cookbook.")
+
+			item = cookbook[itemName]
+			qty = quantity * multiplier
+
+			if item.get("type") == "ingredient":
+				totalCookTime += item.get("cookTime") * qty
+
+				if itemName in totalIngredients:
+					totalIngredients[itemName]["quantity"] += qty
+				else:
+					totalIngredients[itemName] = {"name": itemName, "quantity": qty}
+			else:
+				cookTime, ingredients = dfs(item, qty)
+
+				totalCookTime += cookTime
+
+				for ingredientName, ingredientData in ingredients.items():
+					if ingredientName in totalIngredients:
+							totalIngredients[ingredientName]["quantity"] += ingredientData["quantity"]
+					else:
+							totalIngredients[ingredientName] = ingredientData.copy()
+
+		return totalCookTime, totalIngredients
+
+	try:
+		cookTime, ingredients = dfs(entry)
+		return jsonify({
+			"name": name,
+			"cookTime": cookTime,
+			"ingredients": list(ingredients.values())
+		}), 200
+	except ValueError as e:
+		return str(e), 400
+
+@app.route('/reset', methods=['POST'])
+def reset():
+	cookbook.clear()
+	return {}, 200
 
 # =============================================================================
 # ==== DO NOT TOUCH ===========================================================
